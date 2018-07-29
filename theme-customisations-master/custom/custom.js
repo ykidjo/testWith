42665ea7f7f2f76ccd6d0bcba18363aebf10ec21
ykidjo <yoann.kidjo@gmail.com>
2018-07-29 14:41:01 +0200

    first commit

---
+++ theme-customisations-master/custom/custom.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($){
+	$('.add_to_cart_button').click(function(){
+    var attribute = $(this).attr('data-product_id');
+		
+		/*$('.primary').append(load(localhost/testWith/panier/)).hide().remove($('.cart_item'));*/
+  })
+
+});
