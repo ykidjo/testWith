38454283a403e4ef014bc56e6b58a948d6a021d0
ykidjo <yoann.kidjo@gmail.com>
2018-07-30 16:36:29 +0200

    Add files via upload

--- theme-customisations-master/custom/functions.php
+++ theme-customisations-master/custom/functions.php
@@ -15,3 +15,16 @@ if ( ! defined( 'ABSPATH' ) ) {
  * functions.php
  * Add PHP snippets here
  */
+ //removing all the items from the cart
+
+ add_filter( 'woocommerce_add_cart_item_data', 'wdm_empty_cart', 10,  3);
+
+ function wdm_empty_cart( $cart_item_data, $product_id, $variation_id )
+ {
+
+     global $woocommerce;
+     $woocommerce->cart->empty_cart();
+
+     // Do nothing with the data and return
+     return $cart_item_data;
+ }
